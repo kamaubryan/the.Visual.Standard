2927d31ca62dbc41e128d14bbcfae3404bb473d6
kamaubryan <briankamau344@gmail.com>
2026-04-18 15:34:10 +0300

    the initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "/src/index.css"
+
+const App = () => {
+  return (
+    <div>
+      <p className = "text-3xl"> hello</p>
+      <h1 className="text-grey text-[30px]"> wagwan</h1>
+    </div>
+  )
+}
+
+export default App
